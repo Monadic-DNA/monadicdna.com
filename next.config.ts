dd599e5b5278c034866884ea67653c5326675dff
Jay <jmeattle@shareaholic.com>
2025-03-04 12:48:08 -0500

    Combine configs

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        pathname: '/vi/**',
+      },
+    ],
     domains: [
       'hebbkx1anhila5yf.public.blob.vercel-storage.com'
     ]
