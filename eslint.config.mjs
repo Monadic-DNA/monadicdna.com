7b4d5f3c29c3ec178a289b7ee0cd3a88cd971418
Jay <jmeattle@shareaholic.com>
2025-03-04 12:50:24 -0500

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,7 +13,8 @@ const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript", "prettier"],
     rules: {
-      semi: ["error", "always"],
+      'semi': 'off',
+      '@typescript-eslint/semi': 'off'
     },
   }),
   // ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
